b7134af0a88b5604e828bcfc20b7cf9f4ec42c3b
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-08-16 19:55:19 +0200

    Added section to dashboard

--- packages/app/src/pages/Dashboard.tsx
+++ packages/app/src/pages/Dashboard.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import { RouteComponentProps } from '@reach/router'
+import { Section } from '../components/Section'
 
 export const Dashboard: React.FunctionComponent<RouteComponentProps> = (
   props
-): JSX.Element => <div>Dashboard</div>
+): JSX.Element => (
+  <Section>
+    <p className="is-4">Dashboard</p>
+  </Section>
+)
