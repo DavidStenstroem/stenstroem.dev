ccbf6dc7a18bea8b79df0459e9d9c13234e96a45
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-08-16 19:53:15 +0200

    WIP: Nav component

--- packages/app/src/components/Nav.tsx
+++ packages/app/src/components/Nav.tsx
@@ -1,25 +1,39 @@
 import * as React from 'react'
 import { Link } from '@reach/router'
 import { Logo } from './Logo'
+import { MainContext } from '../context/MainContext'
+import classnames from 'classnames'
 
-export const Nav: React.FunctionComponent = (): JSX.Element => (
-  <nav className="navbar" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <Logo />
-      <a
-        aria-label="menu"
-        aria-expanded="false"
-        className="navbar-burger burger"
-        role="button"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+export const Nav: React.FunctionComponent = (): JSX.Element => {
+  const [open, setOpen] = React.useState<boolean>(false)
+  return (
+    <MainContext.Consumer>
+      {({ account }): JSX.Element => (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <Logo />
+            {account && (
+              <a
+                aria-label="menu"
+                aria-expanded="false"
+                className="navbar-burger burger"
+                role="button"
+                onClick={() => setOpen(!open)}
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            )}
+          </div>
 
-    <div className="navbar-menu">
-      <div className="navbar-start"></div>
-    </div>
-  </nav>
-)
+          {account && (
+            <div className={classnames('navbar-menu', open && 'is-active')}>
+              <div className="navbar-start"></div>
+            </div>
+          )}
+        </nav>
+      )}
+    </MainContext.Consumer>
+  )
+}
