688b40e88ef0b00ce2e93a148b3e1169c121eabc
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-08-17 19:31:53 +0200

    Added logic to resolvers

--- packages/server/src/graphql/account/account.resolvers.ts
+++ packages/server/src/graphql/account/account.resolvers.ts
@@ -1,6 +1,10 @@
 import { Resolvers, Account, FormError } from '../../types/graphql'
 import { authenticate } from '../../authentication'
 import { RequestWithUser } from '../../types/RequestWithUser'
+import { changeNameSchema, changePasswordSchema } from '@stenstroem-dev/shared'
+import { formatError } from '../../utils/formatError'
+import { ValidationError } from 'yup'
+import { pbkdf2Sync, randomBytes } from 'crypto'
 
 export const resolvers: Resolvers = {
   Query: {
@@ -14,12 +18,69 @@ export const resolvers: Resolvers = {
   },
 
   Mutation: {
-    changeName: async (parent, args, context, info): Promise<FormError[]> => [],
+    changeName: async (
+      parent,
+      { newName },
+      { req },
+      info
+    ): Promise<FormError[]> => {
+      const user = await authenticate(req as RequestWithUser)
+      try {
+        await changeNameSchema.validate({ newName }, { abortEarly: false })
+      } catch (err) {
+        return formatError(err as ValidationError)
+      }
+
+      user.name = newName
+      await user.save()
+
+      // todo - update tokens
+
+      return null
+    },
+
     changePassword: async (
       parent,
-      args,
-      context,
+      { input: { currentPassword, newPassword } },
+      { req },
       info
-    ): Promise<FormError[]> => [],
+    ): Promise<FormError[]> => {
+      const user = await authenticate(req as RequestWithUser)
+      try {
+        await changePasswordSchema.validate(
+          { currentPassword, newPassword },
+          { abortEarly: false }
+        )
+      } catch (err) {
+        return formatError(err as ValidationError)
+      }
+
+      const hash = pbkdf2Sync(
+        currentPassword,
+        user.salt,
+        10000,
+        512,
+        'sha512'
+      ).toString('hex')
+      if (hash !== user.hash) {
+        return [{ path: 'currentPassword', message: 'Forkert adgangskode' }]
+      }
+
+      const newSalt = randomBytes(32).toString('hex')
+      const newHash = pbkdf2Sync(
+        newPassword,
+        newSalt,
+        10000,
+        512,
+        'sha512'
+      ).toString('hex')
+
+      user.salt = newSalt
+      user.hash = newHash
+      await user.save()
+
+      // todo - update tokens
+      return null
+    },
   },
 }
