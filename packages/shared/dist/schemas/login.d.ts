c8dc026e7406c7d743323684a22c2a46d52dc030
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-08-17 14:42:24 +0200

    Types for shrared

--- packages/shared/dist/schemas/login.d.ts
+++ packages/shared/dist/schemas/login.d.ts
@@ -1,5 +1,5 @@
 import * as yup from 'yup';
-export declare const loginSchema: yup.ObjectSchema<yup.Shape<{}, {
+export declare const loginSchema: yup.ObjectSchema<yup.Shape<object, {
     email: string;
     password: string;
 }>>;
--- packages/shared/dist/schemas/register.d.ts
+++ packages/shared/dist/schemas/register.d.ts
@@ -1,5 +1,5 @@
 import * as yup from 'yup';
-export declare const registerSchema: yup.ObjectSchema<yup.Shape<{}, {
+export declare const registerSchema: yup.ObjectSchema<yup.Shape<object, {
     name: string;
     email: string;
     password: string;
