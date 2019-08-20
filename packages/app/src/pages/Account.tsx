ffdf8a59a2a191dec7045fcf1f836b40d3cc00f9
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-08-20 20:04:58 +0200

    Removed unused code

--- packages/app/src/pages/Account.tsx
+++ packages/app/src/pages/Account.tsx
@@ -15,20 +15,6 @@ export const Account: React.FunctionComponent<
   const { data, loading, error } = useGetMeQuery()
   if (loading) return <p>Loading ...</p>
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
-  // return (
-  //   <>
-  //     <section className="hero">
-  //       <div className="hero-body">
-  //         <div className="container">
-  //           <h1 className="title">{data.me.name}</h1>
-  //           <h2 className="subtitle">{data.me.email}</h2>
-  //         </div>
-  //       </div>
-  //     </section>
-  //     <ChangeName name={data.me.name} />
-  //     <ChangePassword />
-  //   </>
-  // )
 
   return (
     <Section>
