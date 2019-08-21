3177a394ccc8f456d1878328c3356e685069a2dc
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-08-21 11:57:43 +0200

    Dummy invite page

---
+++ packages/app/src/pages/Invite.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { RouteComponentProps } from '@reach/router'
+
+export const Invite: React.FunctionComponent<RouteComponentProps> = (
+  props
+): JSX.Element => <div>Invite</div>
