45dc1eb6b704c8abfe16450cee5fe1153f7f5e43
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-08-27 11:08:40 +0200

    Placeholder function until utils is complete

--- packages/server/src/upload/index.ts
+++ packages/server/src/upload/index.ts
@@ -6,5 +6,8 @@ export const uploadApi = Router()
 uploadApi.post('/', uploadMiddleware, (req, res, next): void => {
   Promise.all((req.files as Express.Multer.File[]).map((file) => upload(file)))
     .then((results) => res.json(results))
-    .catch((err) => res.status(400).json(err))
+    .catch((err) => {
+      console.log(err)
+      res.status(400).json(err)
+    })
 })
