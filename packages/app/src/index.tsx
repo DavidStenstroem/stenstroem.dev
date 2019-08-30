938e2052a00a65448f60cc9778828a023f2a2852
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-08-30 14:08:28 +0200

    Added Sentry to app

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -3,6 +3,11 @@ import { render } from 'react-dom'
 import { ApolloProvider } from '@apollo/react-components'
 import { client } from './apollo/client'
 import { App } from './App'
+import * as Sentry from '@sentry/browser'
+
+Sentry.init({
+  dsn: 'https://d040229282924b478dda2d1f52dda181@sentry.io/1546746',
+})
 
 const root = document.getElementById('root')
 if (root) {
