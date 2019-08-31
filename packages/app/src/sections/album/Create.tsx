a92e7f8cecd3886ae8d1ad71332c0d499e67eb6c
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-08-31 13:34:58 +0200

    WIP: add modal

--- packages/app/src/sections/album/Create.tsx
+++ packages/app/src/sections/album/Create.tsx
@@ -6,11 +6,16 @@ import { Columns } from '../../components/Columns'
 import { Column } from '../../components/Column'
 import { Formik, Field } from 'formik'
 import { createAlbumSchema } from '@stenstroem-dev/shared'
-import { faPencil } from '../../icons'
+import { faPencil, faUpload, FontAwesomeIcon } from '../../icons'
 import { InputField } from '../../components/InputField'
+import { Modal } from '../../components/Modal'
+import { CreateAlbumContext } from '../../context/CreateAlbumContext'
+import { Account } from '../../models/account.model'
 
 export const Create: React.FC<RouteComponentProps> = (props): JSX.Element => {
   const [files, setFiles] = React.useState<File[]>([])
+  const [showUserModal, setShowUserModal] = React.useState<boolean>(false)
+  const [users, setUsers] = React.useState<Account[]>([])
   const initValues: CreateAlbumInput = {
     files: [],
     description: '',
@@ -20,46 +25,94 @@ export const Create: React.FC<RouteComponentProps> = (props): JSX.Element => {
   }
 
   return (
-    <CreateAlbumComponent>
-      {(onMutate): JSX.Element => (
-        <Section>
-          <Columns isCentered isMobile>
-            <Column mobileWidth={10} tabletWidth={6} desktopWidth={4}>
-              <Formik<CreateAlbumInput>
-                initialValues={initValues}
-                validationSchema={createAlbumSchema}
-                onSubmit={(values, actions): void => {}}
-              >
-                {({ submitForm, isSubmitting, resetForm }): JSX.Element => (
-                  <div>
-                    <Field
-                      name="title"
-                      type="text"
-                      icon={faPencil}
-                      label="Titel"
-                      placeholder="Albummets titel"
-                      component={InputField}
-                    />
-                    <div className="field">
-                      <label className="label">Beskrivelse</label>
-                      <div className="control">
+    <CreateAlbumContext.Provider value={{ users, setUsers }}>
+      <CreateAlbumComponent>
+        {(onMutate): JSX.Element => (
+          <>
+            <Modal
+              show={showUserModal}
+              handleClose={() => setShowUserModal(false)}
+            >
+              <p>USERS!</p>
+            </Modal>
+            <Section>
+              <Columns isCentered isMobile>
+                <Column mobileWidth={10} tabletWidth={6} desktopWidth={4}>
+                  <Formik<CreateAlbumInput>
+                    initialValues={initValues}
+                    validationSchema={createAlbumSchema}
+                    onSubmit={(values, actions): void => {}}
+                  >
+                    {({ submitForm, isSubmitting, resetForm }): JSX.Element => (
+                      <div>
                         <Field
-                          name="description"
-                          className="textarea"
-                          resize="none"
-                          component="textarea"
-                          placeholder="Beskrivelse af albummet"
+                          name="title"
+                          type="text"
+                          icon={faPencil}
+                          label="Titel"
+                          placeholder="Albummets titel"
+                          component={InputField}
                         />
+                        <div className="field">
+                          <label className="label">Beskrivelse</label>
+                          <div className="control">
+                            <Field
+                              name="description"
+                              className="textarea"
+                              resize="none"
+                              component="textarea"
+                              placeholder="Beskrivelse af albummet"
+                            />
+                          </div>
+                        </div>
+                        <p>&nbsp;</p>
+                        <Columns isMobile>
+                          <Column>
+                            <button
+                              className="button is-link is-fullwidth"
+                              onClick={() => setShowUserModal(true)}
+                            >
+                              Begræns adgang
+                            </button>
+                          </Column>
+                          <Column>
+                            <button className="button is-link is-fullwidth">
+                              Vælg billeder
+                            </button>
+                          </Column>
+                        </Columns>
+                        <p>&nbsp;</p>
+                        <div className="field">
+                          <div className="file has-name is-boxed is-centered">
+                            <label className="file-label">
+                              <input
+                                type="file"
+                                name="files"
+                                accept="image/*,video/*"
+                                multiple
+                                className="file-input"
+                              />
+                              <span className="file-cta">
+                                <span className="file-icon">
+                                  <FontAwesomeIcon icon={faUpload} />
+                                </span>
+                                <span className="file-label">Upload filer</span>
+                              </span>
+                              <span className="file-name has-text-centered">
+                                something ...
+                              </span>
+                            </label>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                    <p>&nbsp;</p>
-                  </div>
-                )}
-              </Formik>
-            </Column>
-          </Columns>
-        </Section>
-      )}
-    </CreateAlbumComponent>
+                    )}
+                  </Formik>
+                </Column>
+              </Columns>
+            </Section>
+          </>
+        )}
+      </CreateAlbumComponent>
+    </CreateAlbumContext.Provider>
   )
 }
