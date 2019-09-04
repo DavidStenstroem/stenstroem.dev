a494e3faf818b87650f034bfb192075349c2f56a
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-09-04 19:57:44 +0200

    Add polyfill for resize observe

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -5,16 +5,34 @@ import { client } from './apollo/client'
 import { App } from './App'
 import * as Sentry from '@sentry/browser'
 
+const loadPolyfills = () => {
+  const polyfills = []
+
+  if (!supportsResizeObserver()) {
+    polyfills.push(import('resize-observer-polyfill'))
+  }
+
+  return Promise.all(polyfills)
+}
+
+const supportsResizeObserver = (): boolean =>
+  'ResizeObserver' in window &&
+  'ResizeObserverEntry' in
+    window /*&&
+  'contentRect' in ResizeObserverEntry.prototype*/
+
 Sentry.init({
   dsn: 'https://d040229282924b478dda2d1f52dda181@sentry.io/1546746',
 })
 
-const root = document.getElementById('root')
-if (root) {
-  render(
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>,
-    root
-  )
-}
+loadPolyfills().then(() => {
+  const root = document.getElementById('root')
+  if (root) {
+    render(
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>,
+      root
+    )
+  }
+})
