c6b5ccfb49ab72a4c8e4f1ede6eab25f2d9bdefd
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-09-10 10:38:51 +0200

    Renamed 'sections' to 'views'

--- packages/app/src/views/album/Albums.tsx
+++ packages/app/src/views/album/Albums.tsx
@@ -3,8 +3,6 @@ import { RouteComponentProps } from '@reach/router'
 import { Section } from '../../components/Section'
 
 export const Albums: React.FC<RouteComponentProps> = (props): JSX.Element => {
-  const [files, setFiles] = React.useState<File[]>([])
-
   return (
     <Section>
       <div className="content">
