421a9d614b81ded01a8a42f62cf3f552e2d16dcf
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-09-12 14:26:51 +0200

    Added stream view

--- packages/app/src/pages/Dashboard.tsx
+++ packages/app/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { RouteComponentProps, Link } from '@reach/router'
 import { MyAlbumsOverview } from '../components/MyAlbumsOverview'
 import { SharedAlbumsOverview } from '../components/SharedAlbumsOverview'
+import { StreamOverview } from '../components/StreamOverview'
 
 export const Dashboard: React.FunctionComponent<RouteComponentProps> = (
   props
@@ -10,6 +11,7 @@ export const Dashboard: React.FunctionComponent<RouteComponentProps> = (
     <>
       <MyAlbumsOverview />
       <SharedAlbumsOverview />
+      <StreamOverview />
     </>
   )
 }
