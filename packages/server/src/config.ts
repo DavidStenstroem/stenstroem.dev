f0046e2168557acf79efca44836f7cd63bf9b9d7
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-09-19 14:06:27 +0200

    Use .env instead of plain text secrets

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,3 +1,12 @@
+import { config as dotenvConfig, DotenvConfigOptions } from 'dotenv'
+
+const options: DotenvConfigOptions =
+  (process.env.NODE_ENV as string) === 'production'
+    ? { path: '~/.env.stenstroem-dev-server' }
+    : {}
+
+dotenvConfig()
+
 interface Config {
   dbConnectionString: string
   dbName: string
@@ -16,15 +25,14 @@ interface CloudinaryConfig {
 }
 
 export const config: Config = {
-  engineApiKey: '***REMOVED***',
-  dbConnectionString:
-    '***REMOVED***',
-  dbName: '***REMOVED***',
-  accessTokenSecret: '***REMOVED***',
-  refreshTokenSecret: '***REMOVED***',
+  engineApiKey: process.env.ENGINE_API_KEY as string,
+  dbConnectionString: process.env.MONGODB_URI as string,
+  dbName: process.env.DB_NAME as string,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET as string,
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET as string,
   cloudinary: {
-    apiKey: '***REMOVED***',
-    apiSecret: '***REMOVED***',
+    apiKey: process.env.CLOUDINARY_API_KEY as string,
+    apiSecret: process.env.CLOUDINARY_API_SECRET as string,
     baseEndpoint: 'https://api.cloudinary.com/v1_1',
     uploadPreset: 'stenstroem-dev-upload-preset',
     cloudName: 'stnstrm',
