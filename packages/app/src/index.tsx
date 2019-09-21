70de8da8f516e683e5730fc80016be40b2687621
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-09-21 12:35:51 +0200

    Only use sentry in production

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -4,10 +4,13 @@ import { ApolloProvider } from '@apollo/react-components'
 import { client } from './apollo/client'
 import { App } from './App'
 import * as Sentry from '@sentry/browser'
+import { isProduction } from './config/env'
 
-Sentry.init({
-  dsn: 'https://d040229282924b478dda2d1f52dda181@sentry.io/1546746',
-})
+if (isProduction) {
+  Sentry.init({
+    dsn: 'https://d040229282924b478dda2d1f52dda181@sentry.io/1546746',
+  })
+}
 
 const root = document.getElementById('root')
 if (root) {
