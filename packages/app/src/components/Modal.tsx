d429ce1156f41cfa4969218ea5a50ad1200d3298
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-09-23 11:22:36 +0200

    Add modal base component

--- packages/app/src/components/Modal.tsx
+++ packages/app/src/components/Modal.tsx
@@ -1,30 +1,49 @@
 import * as React from 'react'
 import classnames from 'classnames'
+import { createPortal } from 'react-dom'
 
 interface Props {
-  show: boolean
+  isShowing: boolean
   children: React.ReactChild[] | React.ReactChild
-  handleClose?: () => void
+  hide?: React.Dispatch<React.SetStateAction<boolean>>
   hasCloseButton?: boolean
 }
 
 export const Modal: React.FC<Props> = ({
-  show,
+  isShowing,
   children,
-  handleClose,
+  hide,
   hasCloseButton,
-}): JSX.Element => {
-  return (
-    <div className={classnames('modal', show && 'is-active')}>
-      <div className="modal-background" onClick={handleClose}></div>
-      {children}
-      {hasCloseButton && (
-        <button
-          className="modal-close is-large"
-          aria-label="close"
-          onClick={handleClose}
-        ></button>
-      )}
-    </div>
-  )
-}
+}): JSX.Element =>
+  isShowing
+    ? createPortal(
+        <div className="modal is-active">
+          <div className="modal-background" onClick={() => hide(false)}></div>
+          {children}
+          {hasCloseButton && (
+            <button
+              className="modal-close is-large"
+              aria-label="close"
+              onClick={() => hide(false)}
+            ></button>
+          )}
+        </div>,
+        document.body
+      )
+    : null
+
+// {
+//   return (
+//     <div className={classnames('modal', show && 'is-active')}>
+//       <div className="modal-background" onClick={handleClose}></div>
+//       {children}
+//       {hasCloseButton && (
+//         <button
+//           className="modal-close is-large"
+//           aria-label="close"
+//           onClick={handleClose}
+//         ></button>
+//       )}
+//     </div>
+//   )
+// }
---
+++ packages/app/src/hooks/useModal.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export const useModal = () => {
+  const [isShowing, setIsShowing] = useState<boolean>(false)
+  const toggle = () => setIsShowing(!isShowing)
+
+  return {
+    isShowing,
+    toggle,
+  }
+}
