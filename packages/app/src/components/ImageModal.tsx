7958cfd3af966bcde73c16e7f17b947fdd31c47a
David Hvejsel Stenstrøm <david@stenstroem.dk>
2019-09-29 09:21:28 +0200

    Set width, height when source is video

--- packages/app/src/components/ImageModal.tsx
+++ packages/app/src/components/ImageModal.tsx
@@ -18,7 +18,19 @@ export const ImageModal: React.FC<Props> = ({ source }): JSX.Element => {
         <FontAwesomeIcon color="white" size="3x" icon={faChevronLeft} />
       </span>
       <div className="modal-content">
-        <p className="image">
+        <p
+          className="image"
+          style={
+            source.resourceType === ResourceType.Video
+              ? {
+                  width: `${source.width / 2}px`,
+                  height: `${source.height / 2}px`,
+                  margin: '0 auto',
+                  pointerEvents: 'none',
+                }
+              : {}
+          }
+        >
           {source.resourceType === ResourceType.Image ? (
             <img
               src={`https://res.cloudinary.com/stnstrm/${source.resourceType.toLowerCase()}/upload/${
